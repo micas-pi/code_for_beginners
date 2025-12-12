9dbad52c5c3e829905fb79205ccca722ec9aec70
micas-pi <michalyca@gmail.com>
2025-12-12 17:50:09 +0200

    converted assignment 1 to use functions

--- Python/python_assignment_1.py
+++ Python/python_assignment_1.py
@@ -1,13 +1,21 @@
-#### PYTHON FOR BEGINNERS ASSIGNMENT WEEK 1 MICHAL CASPI 313470551 ####
+# Python course for beginners
+# Week 1
+# assignment by Michal Caspi, id 313470551
 
 # 1. create a data set
 
 import pandas as pd
+import numpy as np
 
-my_data = {"patient": [1, 2, 3, 4, 5, 6],
-           "gender": ["male", "male", "female", "female", "female", "male"],
-           "age": [17, 19, 27, 31, 37, 41],
-           "depression": [True, False, False, True, True, False]}
+subject = np.arange(start = 1, stop = 7, step = 1)
+gender = np.random.choice(a = ("female", "male"), size = 6, replace = True)
+age = np.random.uniform(low = 15, high = 40, size = 6)
+depression = np.random.binomial(n = 1, p = 0.15, size = 6)
+
+my_data = {"subject": subject,
+           "gender": gender,
+           "age": age,
+           "depression": depression}
 
 # 2. create a data frame
 
@@ -16,3 +24,5 @@ df = pd.DataFrame(my_data)
 # 3. export as a csv
 
 df.to_csv("week 1 assignment", index = False)
+
+
