7eba3a985eaa112d58668e25fdd9526ffcb0da77
micas-pi <michalyca@gmail.com>
2025-12-12 16:56:44 +0200

    converted variables to use functions

--- Scripts/assignment 1.R
+++ Scripts/assignment 1.R
@@ -1,9 +1,13 @@
-#### R FOR BEGINNERS WEEK 1 ASSIGNMENT BY MICHAL CASPI 313470551 ####
+# R course for beginners
+# Week 1
+# assignment by Michal Caspi, id 313470551
+
 # 1. create a csv file
-patient = (1:6)
-gender = c("female", "female", "male", "female", "male", "male")
-age = c(16, 22, 31, 18, 39, 31)
-depression = c(TRUE, FALSE, FALSE, TRUE, TRUE, FALSE)
+subject = seq(1, 6, 1)
+gender = sample(c("female", "male"), size = 6, replace = TRUE)
+age = runif(6, min = 15, max = 40)
+depression = rbinom(6, 1, 0.15)
 df = data.frame(patient, gender, age, depression)
 View(df)
 write.csv(df, file = "./R course/Data/df_first_assignment.csv")
+
